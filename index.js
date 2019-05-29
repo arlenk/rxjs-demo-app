7a76c644586e50fb7bf1c6b52fffa385f9bdfbdd
arlenk <arlen.khodadadi@gmail.com>
2019-05-29 18:11:37 -0400

    barebones html & js

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const { range } = rxjs;
-const { map, filter } = rxjs.operators;
+const { range, of, interval } = rxjs;
+const { map, filter, merge, take } = rxjs.operators;
 
-range(1, 200).pipe(
-  filter(x => x % 2 === 1),
-  map(x => x + x)
-).subscribe(x => console.log(x));
+const blah1$ = interval(1000);
+const blah2$ = interval(3000);
 
+const blah3$ = blah1$.pipe(
+    merge(blah2$),
+    take(20)
+)
+
+blah3$.subscribe((x) => {
+    console.log(x);
+})
